d9cc81357b6cc4d1709287d3d314a4ebbdb71689
Pimin Konstantin Kefaloukos <skipperkongen@gmail.com>
2022-02-08 22:38:50 +0100

    fixed bug in setup

--- setup.py
+++ setup.py
@@ -4,31 +4,28 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('requirements.txt') as fi:
-    REQUIRE = [
-        line.strip() for line in fi.readlines()
-        if not line.startswith('#')
-    ]
-
 setup(
     name='georgstage',
-    version='v0.2.10',
+    version='0.2.10',
     download_url='https://github.com/skipperkongen/georgstage/archive/refs/tags/v0.2.10.tar.gz',
-    licence='MIT',
     author="Pimin Konstantin Kefaloukos",
     author_email="skipperkongen@gmail.com",
     description="Hjælpeprogram til skoleskibet Georg Stage",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/skipperkongen/georgstage",
-    install_requires=REQUIRE,
+    install_requires=[
+        'numpy~=1.22.0',
+        'pandas~=1.4.0',
+        'PuLP~=2.6',
+        'pyinstaller~=4.9',
+    ],
     extras_require={'test': ['pytest', 'flake8']},
-    packages=find_packages('georgstage'),
+    packages=find_packages('src'),
     package_dir={'': 'src'},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    py_modules=['georgstage'],
 )
